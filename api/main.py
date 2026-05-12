5a33c8150423a9c66884515175a48ed19b1e0bbe
therealluro <jkwiatkowski@student.neumont.edu>
2026-05-11 23:46:05 -0600

    Oops forgot to do the cors header thing

--- api/main.py
+++ api/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import requests
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # for assignment/dev
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class PredictionRequest(BaseModel):
     features: list
